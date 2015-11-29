a05d7783eedc79d1de6fc543177ea20cf79e7c6c
Justin Obney <justinobney@gmail.com>
2015-11-29 13:55:39 -0500

    feat(properties): scaffold property table

--- screens/PropertyDetail/index.jsx
+++ screens/PropertyDetail/index.jsx
@@ -1,7 +1,9 @@
 // Property Detail Screen
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { pushState } from 'redux-router';
+import { Table } from 'elemental';
+
 import './index.scss';
 
 import ContentPage from 'components/ContentPage';
@@ -18,7 +20,64 @@ export default class PropertyDetail extends Component {
           <h1>Properties</h1>
         </header>
         <main>
-          Property detail here...
+          <Table>
+          	<colgroup>
+              <col width="" />
+              <col width="" />
+              <col width="100" />
+          		<col width="100" />
+          	</colgroup>
+          	<thead>
+          		<tr>
+                <th>Address</th>
+          			<th>Tenany</th>
+          			<th>Rent</th>
+          			<th>Current</th>
+          		</tr>
+          	</thead>
+          	<tbody>
+          		<tr>
+          			<td>
+          				<a href="javascript:;">123 Foo Ave, Denham Springs, LA 70817</a>
+          			</td>
+          			<td>John Smith</td>
+                <td>$700</td>
+          			<td>Yes</td>
+          		</tr>
+              <tr>
+          			<td>
+          				<a href="javascript:;">123 Foo Ave, Denham Springs, LA 70817</a>
+          			</td>
+          			<td>John Smith</td>
+                <td>$700</td>
+          			<td>Yes</td>
+          		</tr>
+              <tr>
+          			<td>
+          				<a href="javascript:;">123 Foo Ave, Denham Springs, LA 70817</a>
+          			</td>
+          			<td>John Smith</td>
+                <td>$700</td>
+          			<td>No</td>
+          		</tr>
+              <tr>
+          			<td>
+          				<a href="javascript:;">123 Foo Ave, Denham Springs, LA 70817</a>
+          			</td>
+          			<td>John Smith</td>
+                <td>$700</td>
+          			<td>Yes</td>
+          		</tr>
+              <tr>
+          			<td>
+          				<a href="javascript:;">123 Foo Ave, Denham Springs, LA 70817</a>
+          			</td>
+          			<td>John Smith</td>
+                <td>$700</td>
+          			<td>Yes</td>
+          		</tr>
+          	</tbody>
+          </Table>
         </main>
       </ContentPage>
     );
